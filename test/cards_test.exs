5aa987ad3054f1c69bd12ee36595ac00c10bea51
Mate Csornyei <mate@csornyei.com>
2021-01-08 22:24:33 +0100

    Basic tests

--- test/cards_test.exs
+++ test/cards_test.exs
@@ -1,8 +1,19 @@
 defmodule CardsTest do
   use ExUnit.Case
   doctest Cards
+  @hand_size 2
+  @test_deck ["a", "b", "c", "d"]
 
-  test "greets the world" do
-    assert Cards.hello() == :world
+  test "test if it return true if card is in the desk" do
+    assert Cards.contains?(@test_deck, "a") == true
+  end
+
+  test "test if it return false if card not in the desk" do
+    assert Cards.contains?(@test_deck, "e") == false
+  end
+
+  test "test if it deal correct amount of cards" do
+    {hand, _rest_of_deck} = Cards.deal(@test_deck, @hand_size)
+    assert length(hand) == @hand_size
   end
 end
