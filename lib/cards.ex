36ed398e85754bf81434d8cd4849b918710564e4
Mate Csornyei <mate@csornyei.com>
2021-01-09 11:21:42 +0100

    Saving to file

--- lib/cards.ex
+++ lib/cards.ex
@@ -23,15 +23,23 @@ defmodule Cards do
     end
   end
 
+  @spec shuffle([any]) :: [[any]]
   def shuffle(deck) do
     Enum.shuffle(deck)
   end
 
+  @spec contains?([any], any) :: boolean
   def contains?(deck, card) do
     Enum.member?(deck, card)
   end
 
+  @spec deal([any], integer) :: {[any], [any]}
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
+  end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
   end
 end
