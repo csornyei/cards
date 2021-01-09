5b9b7986b6ac046c1895f0647e8135ddb7a8a654
Mate Csornyei <mate@csornyei.com>
2021-01-09 12:25:13 +0100

    Doc tests

--- test/cards_test.exs
+++ test/cards_test.exs
@@ -1,19 +1,4 @@
 defmodule CardsTest do
   use ExUnit.Case
   doctest Cards
-  @hand_size 2
-  @test_deck ["a", "b", "c", "d"]
-
-  test "test if it return true if card is in the desk" do
-    assert Cards.contains?(@test_deck, "a") == true
-  end
-
-  test "test if it return false if card not in the desk" do
-    assert Cards.contains?(@test_deck, "e") == false
-  end
-
-  test "test if it deal correct amount of cards" do
-    {hand, _rest_of_deck} = Cards.deal(@test_deck, @hand_size)
-    assert length(hand) == @hand_size
-  end
 end
